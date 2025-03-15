a90e4329c404a3b0bef96cee2c5654a91651ea88
pleaobraga <pleao.braga@gmail.com>
2025-03-15 18:54:51 -0300

    feat: create more stories to calculato-visor

--- apps/web/src/components/feature-components/calculator/components/visor/calculator-visor/calculator-visor.stories.tsx
+++ apps/web/src/components/feature-components/calculator/components/visor/calculator-visor/calculator-visor.stories.tsx
@@ -1,12 +1,17 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { CalculatorVisor } from '.'
+import { OperationVisor } from '../operation-visor'
+import { TemperatureVisor } from '../temperature-visor'
 
 const meta = {
   title: 'Visor/Calculator Visor',
   component: CalculatorVisor,
   parameters: {
     layout: 'centered',
+  },
+  args: {
+    children: 'text',
   },
   tags: ['autodocs'],
 } satisfies Meta<typeof CalculatorVisor>
@@ -18,4 +23,28 @@ export const Default: Story = {
   args: {
     children: 'text',
   },
+}
+
+export const WithOperationVisor: Story = {
+  render: () => (
+    <CalculatorVisor>
+      <OperationVisor
+        result="124"
+        history="12*12"
+      />
+    </CalculatorVisor>
+  ),
+}
+
+export const WithTemperatureVisor: Story = {
+  render: () => (
+    <CalculatorVisor>
+      <TemperatureVisor
+        onChangeFrom={() => {}}
+        onChangeTo={() => {}}
+        value={12}
+        response={34}
+      />
+    </CalculatorVisor>
+  ),
 }
